e6312bf4ce2b20313a7b1db107ff2bfef58d24d3
gjf2a <ferrer@hendrix.edu>
2021-01-27 11:19:22 -0600

    No-mutation version.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use std::env;
 
 fn main() {
-    let mut total = 0.0;
-    for arg in env::args().skip(1) {
-        match arg.parse::<f64>() {
-            Ok(value) => total += value,
-            Err(_) => println!("\"{}\" is not parseable", arg)
-        }
-    }
+    let total = env::args()
+        .skip(1)
+        .map(|v| v.parse::<f64>().unwrap())
+        .sum::<f64>();
     println!("Total: {}", total);
 }
