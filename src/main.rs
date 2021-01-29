6fda365646e5cc8e6c3721ce550bcecedcd20911
gjf2a <ferrer@hendrix.edu>
2021-01-29 08:47:07 -0600

    Multiplication version.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 use std::env;
 
+#[derive(Debug,Copy,Clone)]
+enum Op {
+    Add, Mult
+}
+
+impl Op {
+    fn from_arg(arg: &str) -> Option<Self> {
+        match arg {
+            "-m" => Some(Self::Mult),
+            "-a" => Some(Self::Add),
+            _ => None
+        }
+    }
+
+    fn identity(&self) -> isize {
+        match self {
+            Op::Add => 0,
+            Op::Mult => 1
+        }
+    }
+
+    fn apply(&self, a: isize, b: isize) -> isize {
+        match self {
+            Op::Add => a + b,
+            Op::Mult => a * b
+        }
+    }
+}
+
 fn main() {
-    let total = env::args()
-        .skip(1)
-        .map(|v| v.parse::<f64>().unwrap())
-        .sum::<f64>();
+    let mut operation = Op::Add;
+    let mut total = operation.identity();
+    for arg in env::args().skip(1) {
+        match Op::from_arg(arg.as_str()) {
+            Some(op) => {
+                operation = op;
+                total = operation.identity();
+                println!("{:?}", operation);
+            }
+            None => match arg.parse::<isize>() {
+                Ok(value) => total = operation.apply(total, value),
+                Err(_) => println!("'{}' is not parseable", arg)
+            }
+        }
+    }
     println!("Total: {}", total);
 }
